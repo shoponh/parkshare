d103f5ae684e2958b164959a92e1a639da1af9b9
Shopon <shoponh@yahoo.com>
2016-11-01 19:42:45 -0400

    profile screen - displaying data from server

--- app/src/main/java/com/parkshare/ProfileActivity.java
+++ app/src/main/java/com/parkshare/ProfileActivity.java
@@ -5,14 +5,19 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.parkshare.constant.Constant;
+import com.parkshare.pojo.ItemInfo;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,19 +30,28 @@ public class ProfileActivity extends Activity {
 
     // Progress Dialog Object
     ProgressDialog prgDialog;
-    // Error Msg TextView Object
     TextView errorMsg;
-    // Name Edit View Object
-    EditText nameET;
-    // Email Edit View Object
-    EditText emailET;
-    // Passwprd Edit View Object
-    EditText pwdET;
+
+    EditText person_id;
+    EditText address_id;
+    EditText profile_name;
+    EditText email;
+    EditText password;
+    EditText address1;
+    EditText address2;
+    EditText city;
+    EditText state;
+    EditText zip;
+    TextView country;
+    EditText latitude;
+    EditText longitude;
+    EditText phone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
-
+        setDefaultValues();
 
         /*
         // Find Error Msg Text View control by ID
@@ -45,6 +59,7 @@ public class ProfileActivity extends Activity {
         nameET = (EditText)findViewById(R.id.registerName);
         emailET = (EditText)findViewById(R.id.registerEmail);
         pwdET = (EditText)findViewById(R.id.registerPassword);
+        nameET = (EditText)findViewById(R.id.registerName);
         */
 
         getUserProfile("producer@gmail.com");
@@ -57,7 +72,7 @@ public class ProfileActivity extends Activity {
     /**
      * Method gets triggered when Register button is clicked
      *
-     * @param view
+     * @param email
      */
     public void getUserProfile(String email){
         RequestParams params = new RequestParams();
@@ -104,14 +119,44 @@ public class ProfileActivity extends Activity {
                 try {
                     // JSON Object
                     JSONObject obj = new JSONObject(response);
-                    // When the JSON response has status boolean value assigned with true
-                    if(obj.getBoolean("status")){
-                        // Set Default Values for Edit View controls
-                        setDefaultValues();
+                    if(obj == null || (Constant.FAIL.equalsIgnoreCase(obj.getString("status"))) ){
+                        Toast.makeText(getApplicationContext(), "ERRRRORRRRR!!!!", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    String statusStr = obj.getString("status");
+                    System.out.println(statusStr);
+
+                    if (Utility.isNotNull(statusStr) && statusStr.equalsIgnoreCase(Constant.SUCCESS)) {
+                        JSONArray jsonArr = (JSONArray) obj.get(Constant.PERSONAL_PROFILE);
+
+                        jsonArr.length();
+                        for (int i = 0; i < jsonArr.length(); i++) {
+                            JSONObject currPerson = (JSONObject) jsonArr.get(i);
+                            //String itemId = (String)currItem.get(Constant.ITEM_ID);
+                            //String specInstruction = (String)currItem.get(Constant.ITEM_SPECIAL_INSTRUNCTION);
+                            //String itemAddressId = (String)currItem.get(Constant.ITEM_ADDRESS_ID);
+
+                            person_id.setText(currPerson.getString("id"));
+                            address_id.setText(currPerson.getString("address_id"));
+                            profile_name.setText(currPerson.getString("name"));
+                            email.setText(currPerson.getString("email"));
+                            password.setText(currPerson.getString("password"));
+                            address_id.setText(currPerson.getString("address1"));
+                            address2.setText(currPerson.getString("address2"));
+                            city.setText(currPerson.getString("city"));
+                            state.setText(currPerson.getString("state"));
+                            zip.setText(currPerson.getString("zip"));
+                            country.setText(currPerson.getString("country"));
+                            phone.setText(currPerson.getString("phone"));
+                            latitude.setText(currPerson.getString("latitude"));
+                            longitude.setText(currPerson.getString("longitude"));
+                        }
+
                         // Display successfully registered message using Toast
                         Toast.makeText(getApplicationContext(), "You are successfully registered!", Toast.LENGTH_LONG).show();
 
-                        navigatetoLoginActivity();
+                        //navigatetoLoginActivity();
                     }
                     // Else display error message
                     else{
@@ -147,9 +192,7 @@ public class ProfileActivity extends Activity {
         });
     }
 
-    /**
-     * Method which navigates from Register Activity to Login Activity
-     */
+    /****
     public void navigatetoLoginActivity(){
         Intent loginIntent = new Intent(getApplicationContext(),LoginActivity.class);
         // Clears History of Activity
@@ -157,10 +200,7 @@ public class ProfileActivity extends Activity {
         startActivity(loginIntent);
     }
 
-    /**
-     * Method which navigates from Register Activity to Login Activity
-     */
-    public void navigatetoLoginActivity(View view){
+     public void navigatetoLoginActivity(View view){
         Intent loginIntent = new Intent(getApplicationContext(),LoginActivity.class);
         // Clears History of Activity
         loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -177,14 +217,42 @@ public class ProfileActivity extends Activity {
     public void navigatetoShowIpPortActivity(View view){
         Toast.makeText(getApplicationContext(), "Current IP and Port: "+ IpPortActivity.getIpPort() , Toast.LENGTH_LONG).show();
     }
+    ***/
+
 
     /**
      * Set degault values for Edit View controls
      */
     public void setDefaultValues(){
-        nameET.setText("");
-        emailET.setText("");
-        pwdET.setText("");
+        person_id = (EditText)findViewById(R.id.person_id);
+        address_id = (EditText)findViewById(R.id.address_id);
+        profile_name = (EditText)findViewById(R.id.profile_name);
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        address_id = (EditText)findViewById(R.id.address_id);
+        address2 = (EditText)findViewById(R.id.address2);
+        city = (EditText)findViewById(R.id.city);
+        state = (EditText)findViewById(R.id.state);
+        zip = (EditText)findViewById(R.id.zip);
+        country = (TextView)findViewById(R.id.country);
+        latitude = (EditText)findViewById(R.id.latitude);
+        longitude = (EditText)findViewById(R.id.longitude);
+        phone = (EditText)findViewById(R.id.phone);
+
+        person_id.setText("");
+        address_id.setText("");
+        profile_name.setText("");
+        email.setText("");
+        password.setText("");
+        address_id.setText("");
+        address2.setText("");
+        city.setText("");
+        state.setText("");
+        zip.setText("");
+        country.setText("");
+        latitude.setText("");
+        longitude.setText("");
+        phone.setText("");
     }
 
 }
